65ba62923f78eca0c3d8e40112c10a5e1ae11171
Steven Noreyko <1909661+okyeron@users.noreply.github.com>
2024-08-18 18:16:23 -0500

    Update arc-test.lua

--- arc-test.lua
+++ arc-test.lua
@@ -4,8 +4,6 @@
 
 
 
-
-
 -- ARC TEST 0.0.1
 -- @okyeron
 -- 
@@ -25,14 +23,11 @@ local selectedring = 1
 
 local focus = { x = 0, y = 0, z = 0 }
 local start = 0
-local tiltvals = { x = 0, y = 0, z = 0 }
 
 local pixels = {{},{},{},{}}
 local patterns = {"fade", "chase", "random", 'ripple'}
 local selectedpattern = 1
 local arc_device
-
-local tiltEnable = false
 
 function clearpixels()
   for j = 1, 4 do
@@ -45,22 +40,19 @@ end
 
 -- init function
 function init()
-  local grds = {}
+	local arcs = {}
 
-  connect()
-  print ("arc " .. arc.vports[devicepos].name)
+	connect()
+	print ("arc " .. arc.vports[devicepos].name)
 
---  arc_device:tilt_enable(0,tiltEnable and 1 or 0) -- sensor number	0-7, 1 = on , 
-
-
-  -- Get a list of arc devices
-  for id,device in pairs(arc.vports) do
-    grds[id] = device.name
-  end
+	-- Get a list of arc devices
+	for id,device in pairs(arc.vports) do
+		arcs[id] = device.name
+	end
   
   -- setup params
   
-  params:add{type = "option", id = "arc_device", name = "arc", options = grds , default = 1,
+  params:add{type = "option", id = "arc_device", name = "arc", options = arcs , default = 1,
     action = function(value)
 		arc_device:all(0)
 		arc_device:refresh()
@@ -73,20 +65,9 @@ function init()
 		clearpixels()
 		print ("arc selected " .. arc.vports[devicepos].name)
     end}
-    
---  params:add{type = "option", id = "tilt", name = "Tilt Enable", options = {"off","on"}, default = 1,
---    action = function(value) 
---        arc_device:tilt_enable(0,value-1)
---        if (value == 2) then tiltEnable = true else tiltEnable = false end
---    end}
-
-  -- setup pixel array for oled
-  -- for i = 1, arc_w*arc_h do
-  --   pixels[i] = 0;
-  -- end
-    
-  setup_metros()
-  buildarctable()
+        
+	setup_metros()
+	buildarctable()
 
 end
 
@@ -105,7 +86,6 @@ function setup_metros()
  rippattern.count = (256) + 1
  rippattern.time = 0.01
  rippattern.event = function(stage)
---	print("ripple start")
 	ripp = ripp + 1
 	ripplepattern()
 	arcfrompixels()
@@ -127,12 +107,12 @@ function setup_metros()
 
  -- random pattern metro
  randpattern = metro.init()
--- rpattern.count = 10
+-- rpattern.count = 10  -- enable this to make it run x times
  randpattern.time = 0.5
  randpattern.event = function(stage)
-   rp = rp + 1
-   randompattern()
-   redraw()
+	rp = rp + 1
+	randompattern()
+	redraw()
  end
  
 end
@@ -154,7 +134,6 @@ function allledsval(ring,value)
     for y = 1, 64 do
       pixels[ring][y]=value
     end
-
 end
 
 function allledsoff()
@@ -163,45 +142,43 @@ function allledsoff()
 end
 
 function buildarctable()
-  arctable = {}
-  cnt = 1
-  for x = 1, 4 do
-    for y = 1, 64 do
-        table.insert (arctable, cnt)
-        cnt = cnt +1
-    end
-  end
+	arctable = {}
+	cnt = 1
+	for x = 1, 4 do
+		for y = 1, 64 do
+			table.insert (arctable, cnt)
+			cnt = cnt +1
+		end
+	end
 end 
 
 
 function chasepattern()
- for x = 1, 4 do
-   for y = 1, 64 do
-     yoffset = x-1
-     pidx = y + (yoffset * 64)
-     if cp == pidx then
-       arc_device:led(x, y, 15)
-       pixels[x][y]=15
-       --draw_pixel(x,y,15)
-     else
-       arc_device:led(x, y, 1)
-       pixels[x][y]=0
-       --draw_pixel(x,y,0)
-
-     end
-   end
- end 
- 
+	for x = 1, 4 do
+		for y = 1, 64 do
+			yoffset = x-1
+			pidx = y + (yoffset * 64)
+			if cp == pidx then
+				arc_device:led(x, y, 15)
+				pixels[x][y]=15
+				--draw_pixel(x,y,15)
+			else
+				arc_device:led(x, y, 1)
+				pixels[x][y]=0
+				--draw_pixel(x,y,0)
+			end
+		end
+	end 
 end
 
 function randompattern()
- for x = 1, 4 do
-   for y = 1, 64 do
-       bright = math.random(0,15)
-       pixels[x][y] = bright
-       arc_device:led(x, y, bright)
-   end
- end 
+	for x = 1, 4 do
+		for y = 1, 64 do
+			bright = math.random(0,15)
+			pixels[x][y] = bright
+			arc_device:led(x, y, bright)
+		end
+	end 
 end
 --
 --
@@ -274,174 +251,144 @@ function arc.delta(n, delta)
 end
 
 function arc_key(x, z)
-  focus.x = x
-  focus.z = z
-  
-  if z > 0 then
-  end
-  redraw()
+	focus.x = x
+	focus.z = z
+
+	if z > 0 then
+	end
+	redraw()
 end
 
 
 -- encoder function
 function enc(n, delta)
-  if n == 1 then
-    selectedring = util.clamp (selectedring + delta, 1, 4)
-  end
+	if n == 1 then
+		selectedring = util.clamp (selectedring + delta, 1, 4)
+	end
 
-  if n == 3 then
-    selectedpattern = util.clamp (selectedpattern + delta, 1, #patterns)
-  end
-  if n==2 then
-  	brightness[selectedring] = brightness[selectedring] + delta 
-  	ledval = util.clamp(brightness[selectedring], 0, 15)
---  	print(ledval)
-	allledsval(selectedring,ledval)
-  end
+	if n == 3 then
+		selectedpattern = util.clamp (selectedpattern + delta, 1, #patterns)
+	end
+	
+	if n==2 then
+		brightness[selectedring] = brightness[selectedring] + delta 
+		ledval = util.clamp(brightness[selectedring], 0, 15)
+		--  	print(ledval)
+		allledsval(selectedring,ledval)
+	end
   
-  -- redraw screen
-  redraw()
+	-- redraw screen
+	redraw()
 end
 
 -- key function
 function key(n, z)
-  if n==1 then
+	if n==1 then
+	end
 
-  end
-  
-  if n==2 then
-    if z == 1 then
-      down_time = util.time()
-    else
-      stopallpatterns()
-      hold_time = util.time() - down_time
-      if hold_time < 1 then
-        allledsoff()
-        print("all leds off")
-      elseif hold_time > 1 then
-        allledson()
-        print("all leds on")
-      end
-    end
-  end 
-  if n==3 and z==1 then
-    stopallpatterns()
-    dp = 0
-    cp = 0
-    rp = 0
-    ripp = 0
-    
-    if selectedpattern == 1 then
-		fadepattern()
-    elseif selectedpattern == 2 then
-    	clearpixels()
-		cpattern:start()
-    elseif selectedpattern == 3 then
-    	clearpixels()
-		randpattern:start()
-    elseif selectedpattern == 4 then
-    	clearpixels()
-		rippattern:start()
---    elseif selectedpattern == 5 then
-    end
-  end 
-  
-  -- redraw screen
-  redraw()
+	if n==2 then
+		if z == 1 then
+			down_time = util.time()
+		else
+			stopallpatterns()
+			hold_time = util.time() - down_time
+			if hold_time < 1 then
+				allledsoff()
+				print("all leds off")
+			elseif hold_time > 1 then
+				allledson()
+				print("all leds on")
+			end
+		end
+	end 
+
+	if n==3 and z==1 then
+		stopallpatterns()
+		dp = 0
+		cp = 0
+		rp = 0
+		ripp = 0
+
+		if selectedpattern == 1 then
+			fadepattern()
+		elseif selectedpattern == 2 then
+			clearpixels()
+			cpattern:start()
+		elseif selectedpattern == 3 then
+			clearpixels()
+			randpattern:start()
+		elseif selectedpattern == 4 then
+			clearpixels()
+			rippattern:start()
+		end
+	end 
+
+	-- redraw screen
+	redraw()
 end
-
-
---function draw_pixel(x,y,b)
---  yoffset = y-1
---  pidx = x + (yoffset * arc_w)
---  if pixels[x][y] > 0 then
---  --if focus.x == x and focus.y == y then
---    screen.stroke()
---    screen.level(b)
---  end
---  screen.pixel((x*offset.spacing) + offset.x, (y*offset.spacing) + offset.y)
---  if pixels[x][y] > 0 then
---    screen.stroke()
---    screen.level(1)
---  end
---end
-
---function draw_arc()
---  screen.level(1)
---  offset = { x = 76, y = 2, spacing = 3 }
---  for x=1,arc_w,1 do 
---    for y=1,arc_h,1 do 
---      yoffset = y-1
---      pidx = x + (yoffset * arc_w)
---      
---      draw_pixel(x,y,pixels[x][y])
---    end
---  end
---  screen.stroke()
---end
 
 
 
 -- screen redraw function
 function redraw()
-  local rdeg
- 
-   arcredraw()
+	local rdeg
 
- -- screen: turn on anti-alias
-  --screen.aa(1)
-  screen.line_width(1.0)
-  -- clear screen
-  screen.clear()
-  
-  
-  -- set pixel brightness (0-15)
-  screen.level(15)
-  screen.move(0, 8)
-  screen.text("ARC TEST")
+	arcredraw()
 
-  screen.move(0, 24)
-  screen.text("Ring: ".. selectedring)
+	-- screen: turn on anti-alias
+--	screen.aa(1)
+	screen.line_width(1.0)
+	-- clear screen
+	screen.clear()
 
-	screen.move(66, 8)
+	-- set pixel brightness (0-15)
+	screen.level(15)
+	screen.move(0, 8)
+	screen.text("ARC TEST")
+
+	screen.move(0, 16)
+	screen.text(devicepos .. ": "..arc.vports[devicepos].name )
+
+	screen.move(120, 36)
+	screen.text_right("Ring: ".. selectedring)
+
+	screen.move(0, 36)
+	screen.text("Pattern: ".. patterns[selectedpattern])
+
+
+	screen.move(0, 44)
+	screen.text("Key: "..focus.x..", "..focus.z)
+
+
+	screen.move(0, 52)
 	screen.text("Delta:")
-	
-	screen.move(80, 16)
-	screen.text("1: ".. arcdelta[1])
-	screen.move(80, 24)
-	screen.text("2: ".. arcdelta[2])
-	screen.move(80, 32)
-	screen.text("3: ".. arcdelta[3])
-	screen.move(80, 40)
-	screen.text("4: ".. arcdelta[4])
 
-	screen.move(100, 8)
+	screen.move(36, 52)
+	screen.text_right(arcdelta[1])
+	screen.move(54, 52)
+	screen.text_right(arcdelta[2])
+	screen.move(72, 52)
+	screen.text_right(arcdelta[3])
+	screen.move(90, 52)
+	screen.text_right(arcdelta[4])
+
+	screen.move(0, 60)
 	screen.text("Accum:")
-	
-	screen.move(110, 16)
-	screen.text(accum[1])
-	screen.move(110, 24)
-	screen.text(accum[2])
-	screen.move(110, 32)
-	screen.text(accum[3])
-	screen.move(110, 40)
-	screen.text(accum[4])
 
-  screen.move(0, 33)
-  screen.text("Pattern: ".. patterns[selectedpattern])
-
-  
-  screen.move(0, 51)
-  screen.text("arc Key: "..focus.x..", "..focus.z)
+	screen.move(36, 60)
+	screen.text_right(accum[1])
+	screen.move(54, 60)
+	screen.text_right(accum[2])
+	screen.move(72, 60)
+	screen.text_right(accum[3])
+	screen.move(90, 60)
+	screen.text_right(accum[4])
 
 
-  screen.move(0, 60)
-  screen.text(devicepos .. ": "..arc.vports[devicepos].name )
 
---  draw_arc()
-  
-  -- refresh screen
-  screen.update()
+
+	-- refresh screen
+	screen.update()
 end
 
 -- called on script quit, release memory
